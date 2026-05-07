7a37160cd4332bea0b21f836861dffb2aea13aca
Francoise <frncoisedusabimana2@gmail.com>
2026-05-07 09:33:49 +0200

    signup

--- signup.php
+++ signup.php
@@ -3,17 +3,19 @@ include "db.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $username = $_POST['username'];
+    $firstname = $_POST['firstname'];
+
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$check = "SELECT * FROM users WHERE username='$username'";
+$check = "SELECT * FROM users WHERE =firstname'$firstname'";
 $result = $conn->query($check);
 
 if ($result->num_rows > 0) {
     
     echo "Username already taken! <a href='signup.html'>Try another</a>";
 } else {
-    $sql = "INSERT INTO users (username, password)
-            VALUES ('$username', '$password')";
+   
+    $sql = "INSERT INTO users (firstname, password)
+            VALUES ('$firstname', '$password')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Signup successful! ";
